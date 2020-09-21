334939de7749e042cacd050020f455604efc9f31
VTran <MeeMofu@users.noreply.github.com>
2020-09-21 10:29:56 -0400

    Added search hotel API

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,22 +28,10 @@ const getToken = new Promise(function(resolve, reject) {
         });
 });
 
-// Example Flight search:
-var searchFlightData = {
-    originLocationCode:'BOS',
-    destinationLocationCode:'PAR',
-    departureDate:'2020-10-01',
-    returnDate:'',
-    adults:2,
-    children:0,
-    infants:0,
-    travelClass:'ECONOMY',
-    nonStop:'false',
-    currencyCode:'USD'
-}
+
 
 function searchAirport(cityName){
-    query='?subType=CITY&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=analytics.travelers.score&view=FULL&keyword=' +cityName;
+    query='?subType=AIRPORT&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=analytics.travelers.score&view=FULL&keyword=' +cityName;
     
     fetch('https://test.api.amadeus.com/v1/reference-data/locations'+query,
         {
@@ -82,6 +70,63 @@ function searchAirport(cityName){
                         // Show there's error with server through modal
                     default: 
                         console.log('Something went wrong with searching city'); // Uses modal later on
+                }
+            })
+          }
+    }).catch(function(error){
+        // Display connection error
+    });
+}
+
+function getRecommendedHotel(data){
+    query='?radius=20&radiusUnit=MILE&amenities=&paymentPolicy=NONE&includeClosed=false&bestRateOnly=true&view=NONE&sort=NONE' 
+    
+    if (data.cityCode) query = query + '&cityCode=' + data.cityCode;
+    if (data.checkInDate) query = query + '&checkInDate=' + data.checkInDate;
+    if (data.checkOutDate) query = query + '&checkOutDate=' + data.checkOutDate;
+    if (data.returnDate) query = query + '&returnDate=' + data.returnDate;
+    if (data.roomQuantity) query = query + '&roomQuantity=' + data.roomQuantity;
+    if (data.adults) query = query + '&adults=' + data.adults;
+    if (data.childAges) query = query + '&childAges=' + data.childAges;
+    if (data.currency) query = query + '&currency=' + data.currency;
+   
+    fetch('https://test.api.amadeus.com/v2/shopping/hotel-offers'+query,
+        {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+    }).then(function(response){
+        if (response.ok) {
+            // Check if amadeus return error
+            response.json().then(function(response){
+                // Shows API respond
+                console.log(response);
+            })
+          } else {
+            //   Error responses
+            response.json().then(function(error){
+                // 400 -> bad request
+                // 401 -> need to reauthorize 
+                // 500 -> internal error
+                // console.log(error);
+                switch (error.errors[0].status) {
+                    case 401:
+                            // Authorize error, get a new token
+                            getToken.then(function(response){
+                            token = response;
+                            localStorage.setItem("travelAgencytoken",response);
+                            // Save new token, then recall the function
+                            getRecommendedFlight();
+                        }).catch(function(error){
+                            // Report error to page using modal
+                        })
+                        break;
+                    case 400:
+                        // If conditioned the input fields correctly, this shouldn't be a problem
+                    case 500:
+                        // Show there's error with server through modal
+                    default: 
+                        console.log('Something went wrong with looking for hotels'); // Uses modal later on
                 }
             })
           }
@@ -150,9 +195,6 @@ function getRecommendedFlight(data){
     });
 }
 
-lat = 51.5;
-long = -0.16666;
-
 if (!token) {
     getToken.then(function(response){
         token = response;
@@ -162,9 +204,33 @@ if (!token) {
     })
 }
 
-searchAirport('London');
+// searchAirport('London');
 
+// Example Flight search:
+var searchFlightData = {
+    originLocationCode:'BOS',
+    destinationLocationCode:'PAR',
+    departureDate:'2020-10-01',
+    returnDate:'',
+    adults:2,
+    children:0,
+    infants:0,
+    travelClass:'ECONOMY',
+    nonStop:'false',
+    currencyCode:'USD'
+}
 // getRecommendedFlight(searchFlightData);
 
 
+// Example of Hotel search
+var searchHotelData = {
+    cityCode:'MCO',  // Use airport code here
+    checkInDate : '2020-10-05',
+    checkOutDate : '2020-10-08',
+    roomQuantity : 1,
+    adults : 2,
+    childAges: '14, 12', //  comma seperated, user input
+    currency : 'USD'
+}
 
+getRecommendedHotel(searchHotelData);
