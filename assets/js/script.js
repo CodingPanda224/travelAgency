4682ceb1eb66123d697ef98bbd2ab230c94d102c
Panda <juanck224@gmail.com>
2020-09-20 21:33:51 -0400

    added nav bar and small temp logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,34 @@ function getRecommendedFlight(tries){
 
 getRecommendedFlight(0); // 0 is important, to keep track of # of tries for authentication
 // Can also parse in Object with all of the input fields
+
+
+
+
+
+// NavBar Usage
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
